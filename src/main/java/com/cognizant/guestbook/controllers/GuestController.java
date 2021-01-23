8975aa291e0bae5171dbac773db5fa56e849f8cb
Ofeoritse nanna <nannaofe20@gmail.com>
2021-01-23 13:52:25 -0600

    implemented  functionality for getAllGuest and unit tested

--- src/main/java/com/cognizant/guestbook/controllers/GuestController.java
+++ src/main/java/com/cognizant/guestbook/controllers/GuestController.java
@@ -7,12 +7,26 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class GuestController {
+
+    List<Guest> guestList;
+
+    public GuestController() {
+        this.guestList = new ArrayList<>();
+    }
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/guest")
     public Guest addGuest(@RequestBody Guest guest) {
+        guestList.add(guest);
         return guest;
+    }
+
+    public List<Guest> getAllGuests() {
+        return guestList;
     }
 }
--- src/test/java/com/cognizant/guestbook/controllers/GuestBookControllerUnitTest.java
+++ src/test/java/com/cognizant/guestbook/controllers/GuestBookControllerUnitTest.java
@@ -1,4 +1,55 @@
 package com.cognizant.guestbook.controllers;
 
-public class GuestBookControllerUnitTest {
+import com.cognizant.guestbook.model.Guest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@WebMvcTest
+class GuestBookControllerUnitTest {
+
+   private GuestController guestController;
+
+   @BeforeEach
+   public void init(){
+       guestController = new GuestController();
+   }
+
+    @Test
+    public void addGuestUnitTest() {
+
+        Guest expectedGuest = new Guest("Ofe", "Awesome");
+
+        Guest actualGuest = guestController.addGuest(expectedGuest);
+        assertEquals(expectedGuest,actualGuest);
+
+        Guest expectedGuest2 = new Guest("Hema", "Yay");
+        Guest actualGuest2 = guestController.addGuest(expectedGuest2);
+        assertEquals(expectedGuest2,actualGuest2);
+    }
+
+    @Test
+    public  void getAllGuestsUnitTest(){
+
+        List<Guest> actual = guestController.getAllGuests();
+
+        Guest ofe = new Guest("Ofe", "Awesome");
+        Guest hema = new Guest("Hema", "Yay");
+        guestController.addGuest(ofe);
+        guestController.addGuest(hema);
+
+        List<Guest> expected = new ArrayList<>();
+        expected.add(ofe);
+        expected.add(hema);
+
+        assertEquals(expected, actual);
+
+
+    }
+
 }
